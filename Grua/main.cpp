7794b934d85ca0daa66e8e4bb08bb6f84761a0b9
Jazmine <jazmine.alfaro@ucsp.edu.pe>
2019-09-30 16:37:31 -0500

    Terminado

--- Grua/main.cpp
+++ Grua/main.cpp
@@ -1,7 +1,7 @@
 
 
 #define GLUT_DISABLE_ATEXIT_HACK
-#include <windows.h>
+//#include <windows.h>
 #include <iostream>
 
 #include<GL/glut.h>
@@ -22,200 +22,181 @@ using namespace std;
 # define PI           3.14159265358979323846  // pi 
 float w, h;
 
-GLvoid initGL()
-{
-GLfloat position[] = { 0.0f, 5.0f, 10.0f, 0.0 };
+bool r = false;
+int mx = 0;
+int my = 0;
 
-//enable light : try without it
-glLightfv(GL_LIGHT0, GL_POSITION, position);
-glEnable(GL_LIGHTING);
-//light 0 "on": try without it
-glEnable(GL_LIGHT0);
+float ax = 0.0;
+float ay = 0.0;
+float speed = 0.1;
 
-//shading model : try GL_FLAT
-glShadeModel(GL_SMOOTH);
+void OnMouseClick(int button, int state, int x, int y) {
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+		mx = x;
+		my = y;
+	}
+	else if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
 
-glEnable(GL_DEPTH_TEST);
-
-//enable material : try without it
-glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
-glEnable(GL_COLOR_MATERIAL);
-
-glClearColor(RED, GREEN, BLUE, ALPHA);
+		r = true;
+	}
+	else if (button == GLUT_RIGHT_BUTTON && state == GLUT_UP) {
+		r = false;
+	}
 }
 
-float a = 0.0;
-void tetera(){
-glColor3f(1,0,1);
-glRotatef(a,0,0,1);
-glTranslatef(4,0,0);
-glutSolidTeapot(2.0);
-
-a+=0.15;
-
+void OnMouseMotion(int x, int y) {
+	int dx = mx - x;
+	int dy = my - y;
+	
+	mx = x;
+	my = y;
+	
+	ax += dx * speed;
+	ay += dy * speed;
 }
 
-float t = 0.0;
-float s = 0.0;
-void sphere(){
-t+=0.001;
-s+=0.15;
-glColor3d(0,0,1);
-glTranslatef(8*cos(t),0,0);
-glutSolidSphere(2.0,20,20);
-glColor3d(1,1,1);
-glRotatef(s,0,0,1);
-glTranslatef(10,0,0);
-glutSolidTeapot(2.0);
 
 
+GLvoid initGL(){
+	GLfloat position[] = { 0.0f, 5.0f, 10.0f, 0.0 };
+
+	//enable light : try without it
+	glLightfv(GL_LIGHT0, GL_POSITION, position);
+	glEnable(GL_LIGHTING);
+	//light 0 "on": try without it
+	glEnable(GL_LIGHT0);
+
+	//shading model : try GL_FLAT
+	glShadeModel(GL_SMOOTH);
+
+	glEnable(GL_DEPTH_TEST);
+
+	//enable material : try without it
+	glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+	glEnable(GL_COLOR_MATERIAL);
+
+	glClearColor(RED, GREEN, BLUE, ALPHA);
 }
-
-float d = 0.0;
-float b = 0.0;
-float c = 0.0;
-void system(){
-b += 0.001;
-glTranslatef(8*cos(b),0.0,0.0); 
-glColor3d(0,0,1);
-glutSolidSphere(1,20,20);
-
-c += 0.15;
-glPushMatrix();
-glColor3d(1,1,1);
-glRotatef(c,0,0,1);
-glTranslatef(10,0,0);
-glutSolidTeapot(2.0);
-
-glColor3d(1,1,0);
-glRotatef(c*3,0,1,0);
-glTranslatef(5,0,0);
-//glutSolidTorus(2.0);
-glutSolidTorus(0.5,1.0,20,20);
-glPopMatrix();
-
-glPushMatrix();
-glColor3d(1,1,1);
-glRotatef(c,1,0,0);
-glTranslatef(0,10,0);
-glutSolidCube(2.0);
-glPopMatrix();
-
-}	
-
-float f = 0.0;
-float v = 0.0;
-void solar_system(){
-f+=0.01;
-v+=0.05;
-
-glPushMatrix();
-//sol
-glColor3d(1,1,0);
-glRotatef(v,0,0,1);
-glutSolidSphere(4,8,8);
-
-glPushMatrix();
-//tierra
-glColor3d(0,0,1);
-glRotatef(3*v,0,0,1);
-glTranslatef(10,0,0);
-glRotatef(f,0,0,1);
-glutSolidSphere(2,8,8);
-//luna
-glColor3d(1,1,1);
-glRotatef(1.5*v,0,0,1);
-
-glTranslatef(4,0,0);
-glRotatef(2*f,0,0,1);
-glutSolidSphere(0.5,8,8);
-glPopMatrix();
-
-
-glPushMatrix();
-glColor3d(1,0,0);
-glRotatef(v,0,0,1);
-glTranslatef(18,0,0);
-glRotatef(f,0,0,1);
-glutSolidSphere(1,8,8);
-glPopMatrix();
-
-glPopMatrix();
-}		
+	
+void circulo(float cx, float cy, float r, int num_segments){
+	glBegin(GL_POLYGON);
+	for(int i = 0; i < num_segments; ++i){
+		float theta = 2.0f * PI * float(i) / float(num_segments);//get the current angle
+		float x = r * cosf(theta);//calculate the x component
+		float y = r * sinf(theta);//calculate the y component
+		glVertex3f(x + cx, y + cy,0);//output vertex
+	}
+	
+	glEnd();
+}
 
 void rectangulo(float w, float h){
-	
 	glBegin(GL_POLYGON);
-	glRotatef(50*PI/180,1,0,0);
-	glVertex2f(0,-h/2);
-	glVertex2f(0,h/2);
-	glVertex2f(w,h/2);
-	glVertex2f(w,-h/2);
+	glVertex3f(0,-h/2,0);
+	glVertex3f(0,h/2,0);
+	glVertex3f(w,h/2,0);
+	glVertex3f(w,-h/2,0);
 	glEnd();
 	
 }
 
+float funround(float var){ 
+	float value = (int)(var * 100 + .5); 
+	return (float)value / 100; 
+} 	
+	
+	
+float calcular_x1(float tam, float angle){
+	float x1 = sin(angle*PI/180) * tam;
+	return funround(x1);
+}
 
+float calcular_y1(float x1, float tam){
+	float y1 = pow(tam,2) - pow(x1,2);
+	y1 = sqrt(y1);
+	return funround(y1) ;
+}
 
-
-int pos = 0;
+float angle1 = 45.0 ;
+float angle2 = 30.0 ;
+float tam1 = 5;
+float tam2 = 8;
+float tam3 = 7;
+float g = 1; // grosor
+float len = 6.0;	
+float radio = 1;
+	
 void window_display(){
-
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glOrtho(-w/32, w/32, -h/32, h/32, -25.0f, 25.0f);
-	/*
-	switch(pos){
-	case(1):
-		tetera();
-	break;
-	case(2):
-		sphere();
-	break;
-	case(3):
-		system();
-	break;
-	case(4):
-		solar_system();
-	break;
-	}
-	*/
 
-	glBegin(GL_LINE_LOOP);
-	glColor3d(0,0,255);
-	glVertex2f(0,0);
-	glVertex2f(0,20);
+	float cx1 = calcular_x1(tam2,angle1);
+	float cy1 = calcular_y1(cx1,tam2);
 	
-	glEnd();
-	glBegin(GL_LINE_LOOP);
-	glColor3d(255,0,0);
-	glVertex2f(0,0);
-	glVertex2f(20,0);
+	if(angle1>90 && angle1<=270){
+		cy1 = -cy1;
+	}
 	
-	glEnd();
+	float cx2 = calcular_x1(tam3,angle2);
+	float cy2 = calcular_y1(cx2,tam3);
 	
-	glColor3f(1,0.5,0);
-	rectangulo(4,2);
+	if(angle2>90 && angle2<=270){
+		cy2 = -cy2;
+	}
 	
 	
+	glPushMatrix();
+		glColor3f(1,0.5,0);
+		glTranslatef(-tam1/2,0,0);
+		rectangulo(tam1,g);
+	glPopMatrix();
+
+	
+	glPushMatrix();
+		glColor3f(1,0,1);
+		glRotatef(angle1,0,0,1);
+		rectangulo(tam2,g);
+	glPopMatrix();
 	
 	
 	
 	glPushMatrix();
-	glColor3f(1,1,0);
-	rectangulo(10,1);
-	
-	
-	
-	glutPostRedisplay();
+		glColor3f(0,0,1);
+		glTranslatef(cy1,cx1,0);
+		glRotatef(angle2,0,0,1);
+		rectangulo(tam3,g);
 	glPopMatrix();
 	
+	
+	
+	glPushMatrix();
+	glColor3f(1,1,1);
+	glTranslatef(cy1+cy2,cx1+cx2-len,0);
+	circulo(0,0,radio,50);
+	glPopMatrix();
+	
+	glPushMatrix();
+		glTranslatef(cy1+cy2,cx1+cx2,0);
+		glBegin(GL_LINES);
+			glColor3f(0,1,0);
+			glVertex3f(0,0,0);
+			glVertex3f(0,-len,0);
+		glEnd();
+	glPopMatrix();
+	
+	
+	
+	if(angle1 > 360){
+		angle1 -= 360;
+	}
+	
+	if(angle2 > 360){
+		angle2 -= 360;
+	}
+	
 	glutSwapBuffers();
-
-	glFlush();
 }
 
 void window_reshape(GLsizei width, GLsizei height){
@@ -226,7 +207,7 @@ glViewport(0, 0, width, height);
 
 glMatrixMode(GL_PROJECTION);
 glLoadIdentity();
-glOrtho(0.0f, 300.0f, 0.0f, 300.0f, -1.0f, 1.0f);
+glOrtho(0.0f, 400.0f, 0.0f, 400.0f, -1.0f, 1.0f);
 
 glMatrixMode(GL_MODELVIEW);
 }
@@ -235,17 +216,22 @@ glMatrixMode(GL_MODELVIEW);
 void window_key(int key, int x, int y)
 {
 	if(key == GLUT_KEY_RIGHT){
-		if(pos<5) pos+=1;
-		else if (pos==5) pos=1;
-		
-		cout<<pos<<endl;
+		angle2-= 1.0;
 		window_display();
 	}
 	
 	if(key == GLUT_KEY_LEFT){
-		if(pos>1) pos-=1;
-		
-		cout<<pos<<endl;
+		angle2 += 1.0;
+		window_display();
+	}
+	
+	if(key == GLUT_KEY_UP){
+		angle1 += 1.0;
+		window_display();
+	}
+	
+	if(key == GLUT_KEY_DOWN){
+		angle1 -= 1.0;
 		window_display();
 	}
 	
@@ -258,31 +244,32 @@ void window_idle(){
 }
 	
 	
-	int main(int argc, char **argv)
-	{
-		glutInit(&argc, argv);
-		
-		glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
-		
-		
-		glutInitWindowSize(600, 600);
-		glutInitWindowPosition(0, 0);
-		glutCreateWindow("TP 2 : Transformaciones");
-		
-		
-		initGL();
-		//init_scene();
-		
-		glutDisplayFunc(&window_display);
-		
-		glutReshapeFunc(&window_reshape);
-		
-		glutSpecialFunc(window_key);
-		
-		//function called on each frame
-		glutIdleFunc(&window_idle);
-		
-		glutMainLoop();
-		
-		return 1;
-	}
+int main(int argc, char **argv)
+{
+	glutInit(&argc, argv);
+	
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+	
+	
+	glutInitWindowSize(800, 800);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("Grúa");
+	
+	
+	initGL();
+	//init_scene();
+	
+	glutDisplayFunc(&window_display);
+	
+	glutReshapeFunc(&window_reshape);
+	
+	glutSpecialFunc(window_key);
+	glutMouseFunc(&OnMouseClick);
+	glutMotionFunc(&OnMouseMotion);
+	//function called on each frame
+	glutIdleFunc(&window_idle);
+	
+	glutMainLoop();
+	
+	return 1;
+}
